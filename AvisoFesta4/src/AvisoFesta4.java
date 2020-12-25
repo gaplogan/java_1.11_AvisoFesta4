d5fcd00f1e7b7d7aec9c8d230e326b748103119b
Gabriel <gaplogan@gmail.com>
2020-12-25 14:31:35 -0300

    2020.12.25.14.31

---
+++ AvisoFesta4/src/AvisoFesta4.java
@@ -0,0 +1,9 @@
+public class AvisoFesta4 {
+    public static void main(String[] args) throws Exception {
+        for (int j = 0; j < 5; j++)
+        { //início do loop
+            //escreve a frase
+            System.out.println("Hoje vai ter festa na casa da Fabi");
+        } //fim do loop
+    }
+}
